535df213b1c9184838102546bdde198021a3a0c1
Abdlwahid <natabd005@gmail.com>
2022-03-15 08:57:46 +0300

    My commit message

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
  /**
  *main - Entry point
- *
- *Return: Always 0 (Success)
+ *My hello. print task
+ *Return: return 1 after progress execution
  */
 int main(void)
 {
-	printf("with proper grammer, but the outcome is a piece of art,\n");
-	return (0);
+printf("with proper grammer, but the outcome is a piece of art,\n");
+return (0);
 }
