0707dd4b725d74384c1dffb15f81374b60178acc
Abdlwahid <natabd005@gmail.com>
2022-06-09 23:48:43 +0300

    0-print_dlistint.c

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,30 +1,30 @@
 #include "lists.h"
 
 /**
-* print_dlistint - print a double linked list
-* @h: header of double linked list
-* Return: number of nodes
-*/
+ * print_dlistint - prints all the elements of a dlistint_t list
+ * @h: pointer to head of the dlistint_t list
+ * Return: number of nodes of dlistint_t list
+ */
+
+
 size_t print_dlistint(const dlistint_t *h)
 {
-	size_t i = 0;
-	int iniprint = 0;
+	size_t num_of_nodes = 0;
+	dlistint_t *current_node;
 
-	for (i = 0; h != NULL; i++)
+	if (!h)
+		return (num_of_nodes);
+
+	printf("%d\n", h->n);
+	num_of_nodes++;
+	current_node = h->next;
+
+	while (current_node)
 	{
-		if (h->prev != NULL && iniprint == 0)
-		{
-			h = h->prev;
-			continue;
-		}
-		else if (h->prev == NULL)
-		{
-			iniprint = 1;
-			i = 0;
-		}
-		printf("%i\n", h->n);
-		h = h->next;
-
+		printf("%d\n", current_node->n);
+		num_of_nodes++;
+		current_node = current_node->next;
 	}
-	return (i);
+
+	return (num_of_nodes);
 }
